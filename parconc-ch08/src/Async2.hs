24361fb26c7ee6de570da1f5ada120ec8a7a3ead
Damian Nadales <damian.nadales@gmail.com>
2016-06-07 08:18:05 +0200

    Added solutions for Async2

--- parconc-ch08/src/Async2.hs
+++ parconc-ch08/src/Async2.hs
@@ -11,12 +11,17 @@ data Async a = Async (MVar (Either SomeException a))
 async :: IO a -> IO (Async a)
 async act = do
   mvar <- newEmptyMVar
-  _ <- forkIO $ do result <- act; putMVar mvar (Right result)
+  _ <- forkIO $ do
+    result <- try act
+    putMVar mvar result
   return (Async mvar)
 
 wait :: Async a -> IO a
-wait (Async mvar) = undefined
-  -- readMVar mvar
+wait (Async mvar) = do
+  result <- readMVar mvar
+  case result of
+    Left e -> throw e
+    Right a -> return a
 
 waitCatch :: Async a -> IO (Either SomeException a)
-waitCatch = undefined
+waitCatch (Async mvar) = readMVar mvar
