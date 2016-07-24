346c994f2ae27149882f417a2690a55a614582a0
Damian Nadales Agut <damian.nadales@gmail.com>
2016-07-24 17:34:00 +0200

    Wrote an implementation that passes the unit tests.

--- parconc-ch10/src/Barrier.hs
+++ parconc-ch10/src/Barrier.hs
@@ -1,5 +1,5 @@
 -- |
-module Barrier where
+module Barrier (Barrier, newBarrier, barrier) where
 
 import           Control.Concurrent.STM
 
@@ -8,20 +8,26 @@ type Barrier = TVar BarrierCounter
 
 -- | `newBarrier n` creates a new barrier for `n` threads.
 newBarrier :: Int -> IO (Barrier)
-newBarrier n = atomically $ newTVar $ BarrierCounter 0 0
+newBarrier n = atomically $ newTVar $ BarrierCounter n 0
 
--- | `barrier b` suspends the thread till all the threads are at barrier `b`.
-barrierSTM :: Barrier -> STM ()
--- The code below won't work. The reason is that the STM transaction is rolled
--- back at the `retry`. Can you see this?
-barrierSTM barrier = do
+-- | Increments the barrier counter.
+--
+-- Note that we want this method to be private, since otherwise threads could
+-- tamper with the barrier's invariant.
+inc :: Barrier -> IO ()
+inc barrier = atomically $ do
   b <- readTVar barrier
   let newCounter = BarrierCounter (total b) (current b + 1)
   writeTVar barrier newCounter
-  if (total newCounter == current newCounter)
+
+-- | `barrier b` suspends the thread till all the threads are at barrier `b`.
+waitAtBarrier :: Barrier -> STM ()
+waitAtBarrier barrier = do
+  counter <- readTVar barrier
+  if (total counter == current counter)
     then return ()
     else retry
 
 
 barrier :: Barrier -> IO ()
-barrier = atomically . barrierSTM
+barrier b = inc b >> (atomically $ waitAtBarrier b)
