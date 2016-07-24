162636d45e1571c02bfe37d88b4d4bcabbf6e569
Damian Nadales Agut <damian.nadales@gmail.com>
2016-07-24 17:23:54 +0200

    Added a wrong implementation. Useful to show in a course.

--- parconc-ch10/src/Barrier.hs
+++ parconc-ch10/src/Barrier.hs
@@ -1,12 +1,27 @@
 -- |
 module Barrier where
 
-data Barrier = Barrier
+import           Control.Concurrent.STM
+
+data BarrierCounter = BarrierCounter {total :: Int, current :: Int}
+type Barrier = TVar BarrierCounter
 
 -- | `newBarrier n` creates a new barrier for `n` threads.
 newBarrier :: Int -> IO (Barrier)
-newBarrier n = return Barrier
+newBarrier n = atomically $ newTVar $ BarrierCounter 0 0
 
 -- | `barrier b` suspends the thread till all the threads are at barrier `b`.
+barrierSTM :: Barrier -> STM ()
+-- The code below won't work. The reason is that the STM transaction is rolled
+-- back at the `retry`. Can you see this?
+barrierSTM barrier = do
+  b <- readTVar barrier
+  let newCounter = BarrierCounter (total b) (current b + 1)
+  writeTVar barrier newCounter
+  if (total newCounter == current newCounter)
+    then return ()
+    else retry
+
+
 barrier :: Barrier -> IO ()
-barrier _ = return ()
+barrier = atomically . barrierSTM
