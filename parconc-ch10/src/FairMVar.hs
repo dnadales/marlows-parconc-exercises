0ec4d8e8a60d079446e495a6398dd17d8bccea67
Damian Nadales Agut <damian.nadales@gmail.com>
2016-07-30 15:49:50 +0200

    Added an implementation of fair MVars. However, this won't pass the unit tests, since the tests themselves are not testing for fairness actually.

--- parconc-ch10/src/FairMVar.hs
+++ parconc-ch10/src/FairMVar.hs
@@ -2,3 +2,40 @@
 
 module FairMVar where
 
+import           Control.Concurrent
+import           Control.Concurrent.STM
+
+data FairMVar a = FairMVar { getValue :: TVar (Maybe a)
+                           , getQueue :: TVar [ThreadId]}
+
+newEmptyFairMVar :: IO (FairMVar a)
+newEmptyFairMVar = do
+  tv <- atomically $ newTVar Nothing
+  tq <- atomically $ newTVar []
+  return (FairMVar tv tq)
+
+takeFairMVar :: FairMVar a -> IO a
+takeFairMVar (FairMVar tv tq) = do
+  mTid <- myThreadId
+  atomically $ do
+    qs <- readTVar tq
+    writeTVar tq (qs ++ [mTid])
+  atomically $ do
+    qs <- readTVar tq
+    -- Invariant: the queue always have at least one element
+    if (head qs == mTid) then
+      do mv <- readTVar tv
+         case mv of
+           Nothing -> retry
+           Just v -> do
+             writeTVar tv Nothing
+             writeTVar tq (tail qs)
+             return v
+    else retry
+
+putFairMVar :: FairMVar a -> a -> IO ()
+putFairMVar (FairMVar tv tq) x = atomically $ do
+  mv <- readTVar tv
+  case mv of
+    Nothing -> writeTVar tv (Just x)
+    _ -> retry
--- parconc-ch10/test/FairMVarSpec.hs
+++ parconc-ch10/test/FairMVarSpec.hs
@@ -2,6 +2,7 @@
 
 module FairMVarSpec where
 
+import           Control.Concurrent
 import           Control.Concurrent.Async
 import           Control.Concurrent.MVar
 import           Control.Exception
@@ -22,12 +23,12 @@ elementsPerConsumer = numElements `quot` numConsumers
 -- Value to put.
 value = 15
 
-consumeMVar :: MVar Int -> [Int] -> IO [Int]
+consumeMVar :: FairMVar Int -> [Int] -> IO [Int]
 consumeMVar mv buf =
   -- We return the contents of the buffer upon receiving an asynchronous
   -- exception.
   handle ((\_ -> return buf) :: (AsyncException -> IO [Int])) $ do
-  v <- takeMVar mv
+  v <- takeFairMVar mv
   consumeMVar mv (v:buf)
 
 spec :: Spec
@@ -35,24 +36,29 @@ spec = do
   describe "Fair mvars" $ do
     -- TODO: This is an exercise for chapter 7
     it "behaves fairly with one consumer" $ do
-      mv <- newEmptyMVar
-      producer <- async $ sequenceA_ $ replicate numElements (putMVar mv value)
-      consumer <- async $ sequenceA $ replicate numElements (takeMVar mv)
+      -- mv <- newEmptyMVar
+      mv <- newEmptyFairMVar
+      producer <- async $ sequenceA_ $ replicate numElements (putFairMVar mv value)
+      consumer <- async $ sequenceA $ replicate numElements (takeFairMVar mv)
       _ <- wait producer
       xs <- wait consumer
       xs `shouldBe` replicate numElements value
 
     it "behaves fairly with multiple consumers" $ do
-      mv <- newEmptyMVar
+      mv <- newEmptyFairMVar
+      producer <- async $ sequenceA_ $ replicate numElements (putFairMVar mv value)
       consumers <- sequenceA $ replicate numConsumers (async $ consumeMVar mv [])
-      producer <- async $ sequenceA_ $ replicate numElements (putMVar mv value)
       _ <- wait producer
       -- Cancel the consumers
+      -- | FIXME: we should make sure that all the consumers are blocked on an
+      -- empty MVar. I don't know how this can be done in a simple way. Waiting
+      -- is the only idea I have.
+      threadDelay (500 * (10^3))
       mapM_ cancel consumers
       results <- mapM wait consumers
-      -- Check that all the consumers have consumed the same number of elements
-      -- (all (== elementsPerConsumer) (map length (results))) `shouldBe` True
+      -- Check that all the elemnts have been consumed.
       sum (map length (results)) `shouldBe` numElements
       let expectedList =
             replicate numConsumers elementsPerConsumer
+      -- Check that all consumers have consumed the same number of elements.
       map length (results) `shouldBe` expectedList
